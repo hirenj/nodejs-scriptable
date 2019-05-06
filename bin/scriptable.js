cd7eb22b2a2b6ca6b76685f83a26a20fb1adcef0
Hiren Joshi <hirenj@gmail.com>
2019-05-06 15:42:35 +0200

    Search for scripts

--- bin/scriptable.js
+++ bin/scriptable.js
@@ -1,3 +1,17 @@
 #! /usr/bin/env node
 
+const fs = require('fs');
+const os = require('os');
+
+const script = process.argv[2];
+
+const script_home = '~/Library/Mobile Documents/iCloud~dk~simonbs~Scriptable/Documents'.replace(/^~/, os.homedir());
+
+fs.readdirSync(script_home).forEach(file => {
+  if ( `${script}.js` === file ) {
+    process.argv[2] = `${script_home}/${file}`;
+  }
+});
+
 require('..');
+
